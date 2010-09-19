78ccd9a1879e989ea00f431d09a9ac13c4c00648
Granit <granit@gratop.(none)>
2010-09-19 23:48:20 +0300

    fix2 in modules - спец. добавлено to_s для выборок из бд

--- app/models/kontakt.rb
+++ app/models/kontakt.rb
@@ -14,14 +14,14 @@ has_and_belongs_to_many :tags
 
 	def self.search(search, user_id)
 	  if search
-		find(:all, :conditions => ["name LIKE ? AND user_id = ?", "%#{search}%", user_id.to_i])
+		find(:all, :conditions => ["name LIKE ? AND user_id = ?", "%#{search}%", user_id.to_s])
 	  else
-		find(:all, :conditions => ["user_id = ?", user_id.to_i])
+		find(:all, :conditions => ["user_id = ?", user_id.to_s])
 	  end
 	end
 
 	def self.find_tag(tag_id, user_id)
-		find(:all, :conditions => ["tag_id = ? AND user_id in (?)", tag_id.to_i, user_id.to_i])
+		find(:all, :conditions => ["tag_id = ? AND user_id in (?)", tag_id.to_s, user_id.to_s])
 	end
 	
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,13 +6,13 @@ has_and_belongs_to_many :kontakts
 	
 	def self.search(search, user_id)
 	  if search
-		find(:all, :conditions => ["name LIKE ? AND user_id = ?", "%#{search}%", user_id.to_i])
+		find(:all, :conditions => ["name LIKE ? AND user_id = ?", "%#{search}%", user_id.to_s])
 	  else
-		find(:all, :conditions => ["user_id = ?", user_id.to_i])
+		find(:all, :conditions => ["user_id = ?", user_id.to_s])
 	  end
 	end
 	
 	def self.find_tag(id, user_id)
-		find(:all, :conditions => ["id = ? AND user_id = ?", id.to_i, user_id.to_i])
+		find(:all, :conditions => ["id = ? AND user_id = ?", id.to_s, user_id.to_s])
 	end
 end
